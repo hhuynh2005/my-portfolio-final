e90c7b44dce47b58695d7796c701b232e69585e4
hhuynh2005 <hn7456401@gmail.com>
2025-11-15 11:48:26 +0700

    fix: Update portfolio title and favicon paths

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,12 +59,12 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Jigar Sable";
-            $("#favicon").attr("href", "assets/images/favicon.png");
+            document.title = "Portfolio | Nguyen Van Huynh";
+            $("#favicon").attr("href", ".assets/images/favicon.png");
         }
         else {
             document.title = "Come Back To Portfolio";
-            $("#favicon").attr("href", "assets/images/favhand.png");
+            $("#favicon").attr("href", ".assets/images/anh1.png");
         }
     });
 
@@ -112,7 +112,7 @@ function showProjects(projects) {
     projects.slice(0, 10).filter(project => project.category != "android").forEach(project => {
         projectHTML += `
         <div class="box tilt">
-      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+      <img draggable="false" src="./assets/images/projects/${project.image}.png" alt="project" />
       <div class="content">
         <div class="tag">
         <h3>${project.name}</h3>
